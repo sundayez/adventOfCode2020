4a5c6ce8a882688fda501300f1eec08f04aa5716
sundayez <sundayez@gmail.com>
2020-12-03 22:54:32 +0100

    Day 3

--- src/com/bernar/adventofcode2020/Day3.scala
+++ src/com/bernar/adventofcode2020/Day3.scala
@@ -6,21 +6,21 @@ object Day3 extends App {
   val h = lines.length
 
   println(countTrees(3, 1))
-  println("=====")
-  println(countTrees(1, 2))
+  println(countTrees(1, 1) * countTrees(3, 1) *
+    countTrees(5, 1) * countTrees(7, 1) * countTrees(1, 2))
 
   private def countTrees(hStep: Int, wStep: Int): Int = {
-    println(w, h)
-    println(Range(0, h, wStep).toList)
-    println(Range(0, hStep*h, hStep).map(e => e % w).toList)
-    println(Range(0, h, wStep).length)
-    println(Range(0, hStep*h, hStep).map(e => e % w).length)
-    println(Range(0, h).zip(Range(0, 3*h, 3).map(e => e % w)))
-
-    Range(0, h).zip(Range(0, hStep*h, 3).map(e => e % w)).count(
-      r => lines(r._1)(r._2) == '#'
-    )
+    Range(0, h, wStep).zip(Range(0, hStep * h, hStep).map(e => e % w)).count(r => lines(r._1)(r._2) == '#')
   }
 }
+
+//    println(w, h)
+//    println(Range(0, h, wStep).toList)
+//    println(Range(0, hStep * h, hStep).map(e => e % w).toList)
+//    println(Range(0, h, wStep).length)
+//    println(Range(0, hStep * h, hStep).map(e => e % w).length)
+//    println(Range(0, h, wStep).zip(Range(0, hStep * h, hStep).map(e => e % w)))
+//    println(Range(0, h, wStep).zip(Range(0, hStep * h, hStep).map(e => e % w)).length)
+
 
 
